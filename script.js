98ec3f089a06b60a2ef53a2c74c55623b3439511
SpectreAscended <94819487+SpectreAscended@users.noreply.github.com>
2022-04-15 01:37:11 -0600

    Refactored code a little bit

--- script.js
+++ script.js
@@ -5,37 +5,32 @@ const button = document.querySelector('button');
 const body = document.querySelector('body');
 const text = document.querySelector('h1');
 
+// Returns a negative character randomly to be used in our transform: translate
+
+const posNeg = function() {
+    let posNeg = Math.random() * 10;
+    return posNeg > 5 ? posNeg = '' : posNeg = '-';
+};
+
+// Make button dart around
+
 btn.addEventListener('mouseover', function(e) {
-    let posNeg1 = Math.random() * 10;
-    let posNeg2 =  Math.random() * 10;
-    posNeg1 > 5 ? posNeg1 = '' : posNeg1 = '-';
-    posNeg2 > 5 ? posNeg2 = '' : posNeg2 = '-';
-    const ranNum = Math.random() * 300 + 100
+    const ranNum = Math.random() * 300 + 100;
     console.log(ranNum);
     console.log('hey');
-    btn.style.transform = `translate(${posNeg1}${ranNum}px, ${posNeg2}${ranNum}px)`
+    btn.style.transform = `translate(${posNeg()}${ranNum}px, ${posNeg()}${ranNum}px)`;
 });
+
+// Display pleasant victory message
 
 btn.addEventListener('click', function(e) {
     e.preventDefault();
-    
-   
-
-   
-
 
     btn.classList.add('hidden');
-    text.classList.remove('hidden')
+    text.classList.remove('hidden');
     setInterval(e => {
         let ranNum = Math.random() * 255;
-        let posNeg1 = Math.random() * 10;
-        let posNeg2 =  Math.random() * 10;
-        posNeg1 > 5 ? posNeg1 = '' : posNeg1 = '-';
-        posNeg2 > 5 ? posNeg2 = '' : posNeg2 = '-';
-        // console.log(ranNum);
-        body.style.backgroundColor = `rgba(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, 1)`
-        text.style.transform = `translate(${posNeg1}${ranNum}px, ${posNeg2}${ranNum}px)`
-    }, 500);
-
-    
-})
+        body.style.backgroundColor = `rgba(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, 1)`;
+        text.style.transform = `translate(${posNeg()}${ranNum}px, ${posNeg()}${ranNum}px)`;
+    }, 250);    
+});
